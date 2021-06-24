daf1c4de5775bba088428db1484a155580f318ef
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2021-06-24 11:49:01 +0200

    Use scalar results in ORM

--- core-bundle/src/Backend/PassEditionStatistics.php
+++ core-bundle/src/Backend/PassEditionStatistics.php
@@ -19,7 +19,6 @@ use Contao\System;
 use DateInterval;
 use DatePeriod;
 use DateTime;
-use Doctrine\DBAL\Connection;
 use Ferienpass\CoreBundle\Entity\Attendance;
 use Ferienpass\CoreBundle\Repository\AttendanceRepository;
 use Ferienpass\CoreBundle\Repository\OfferRepository;
@@ -28,16 +27,14 @@ use Twig\Environment as TwigEnvironment;
 
 class PassEditionStatistics
 {
-    private Connection $connection;
     private TwigEnvironment $twig;
     private TranslatorInterface $translator;
 
     private OfferRepository $offerRepository;
     private AttendanceRepository $attendanceRepository;
 
-    public function __construct(Connection $connection, TwigEnvironment $twig, TranslatorInterface $translator, OfferRepository $offerRepository, AttendanceRepository $attendanceRepository)
+    public function __construct(TwigEnvironment $twig, TranslatorInterface $translator, OfferRepository $offerRepository, AttendanceRepository $attendanceRepository)
     {
-        $this->connection = $connection;
         $this->twig = $twig;
         $this->translator = $translator;
         $this->offerRepository = $offerRepository;
@@ -74,16 +71,14 @@ class PassEditionStatistics
 
     private function countParticipants(int $passEdition): int
     {
-        $count = $this->attendanceRepository->createQueryBuilder('a')
+        return (int) $this->attendanceRepository->createQueryBuilder('a')
             ->select('COUNT(DISTINCT a.participant) AS count')
             ->innerJoin('a.offer', 'o')
             ->andWhere('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->getQuery()
-            ->getSingleResult()
+            ->getSingleScalarResult()
         ;
-
-        return $count['count'];
     }
 
     private function countAttendancesByCategory(int $passEdition): ?array
@@ -100,13 +95,13 @@ class PassEditionStatistics
             ->andWhere('a.status <> :status')
             ->setParameter('status', Attendance::STATUS_WITHDRAWN)
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
         $countConfirmed = (clone $qb)
             ->andWhere('a.status = :status')
             ->setParameter('status', Attendance::STATUS_CONFIRMED)
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
 
         // Transform array to key=>value structure
@@ -126,48 +121,42 @@ class PassEditionStatistics
 
     private function countOffersWithVariants(int $passEdition): int
     {
-        $count = $this->offerRepository->createQueryBuilder('o')
+        return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(o.id) AS count')
             ->andWhere('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->getQuery()
             ->getSingleResult()
         ;
-
-        return $count['count'];
     }
 
     private function countOffersWithoutVariants(int $passEdition): int
     {
-        $count = $this->offerRepository->createQueryBuilder('o')
+        return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(o.id) AS count')
             ->andWhere('o.variantBase IS NULL')
             ->andWhere('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->getQuery()
-            ->getSingleResult()
+            ->getSingleScalarResult()
         ;
-
-        return $count['count'];
     }
 
     private function countHostsWithOffer(int $passEdition): int
     {
-        $count = $this->offerRepository->createQueryBuilder('o')
+        return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(DISTINCT h.id) AS count')
             ->innerJoin('o.hosts', 'h')
             ->andWhere('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->getQuery()
-            ->getSingleResult()
+            ->getSingleScalarResult()
         ;
-
-        return $count['count'];
     }
 
     private function countAttendances(int $passEdition): int
     {
-        $count = $this->attendanceRepository->createQueryBuilder('a')
+        return (int) $this->attendanceRepository->createQueryBuilder('a')
             ->select('COUNT(a.id) AS count')
             ->innerJoin('a.offer', 'o')
             ->andWhere('o.edition = :edition')
@@ -175,10 +164,8 @@ class PassEditionStatistics
             ->andWhere('a.status <> :status')
             ->setParameter('status', Attendance::STATUS_WITHDRAWN)
             ->getQuery()
-            ->getSingleResult()
+            ->getSingleScalarResult()
         ;
-
-        return $count['count'];
     }
 
     private function countAttendancesByDay(int $passEdition): array
@@ -194,7 +181,7 @@ class PassEditionStatistics
             ->groupBy('day')
             ->orderBy('day')
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
 
         if ([] === $daysAndCount) {
@@ -243,7 +230,7 @@ class PassEditionStatistics
             ->setParameter('edition', $passEdition)
             ->groupBy('a.status')
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
 
         // Transform array to key=>value structure
@@ -264,26 +251,36 @@ class PassEditionStatistics
     private function countAttendancesByParticipantAge(int $passEdition): array
     {
         $ageAndCount = $this->attendanceRepository->createQueryBuilder('a')
-            ->select("(CASE WHEN (a.age IS NULL) THEN 'N/A' ELSE a.age END) as age")
-            ->addSelect('COUNT(a.id) as count')
+            ->select(<<<'SQL'
+(
+    CASE
+        WHEN (p.dateOfBirth IS NULL AND a.age IS NOT NULL) THEN a.age
+        WHEN (p.dateOfBirth IS NOT NULL) THEN TIMESTAMPDIFF(YEAR, p.dateOfBirth, ANY_VALUE(d.begin))
+        ELSE 'N/A' END
+    ) as age
+SQL
+            )->addSelect('COUNT(a.id) as count')
             ->innerJoin('a.offer', 'o')
+            ->leftJoin('a.participant', 'p')
+            ->leftJoin('o.dates', 'd')
             ->where('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->andWhere('a.status <> :status')
             ->setParameter('status', Attendance::STATUS_WITHDRAWN)
-            ->groupBy('a.age')
+            ->orderBy('age')
+            ->groupBy('age', 'p.dateOfBirth')
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
 
         // Transform array to key=>value structure
         $ageAndCount = array_combine(array_column($ageAndCount, 'age'), array_column($ageAndCount, 'count'));
 
         $return = [];
-        foreach ($ageAndCount as $k) {
+        foreach ($ageAndCount as $age => $count) {
             $return[] = [
-                'title' => is_numeric($k['age']) ? sprintf('%d Jahre', $k['age']) : $k['age'],
-                'count' => (int) $k['count'],
+                'title' => is_numeric($age) ? sprintf('%d Jahre', $age) : $age,
+                'count' => (int) $count,
             ];
         }
 
@@ -297,10 +294,10 @@ class PassEditionStatistics
     private function getUtilization(int $passEdition): array
     {
         $result = $this->attendanceRepository->createQueryBuilder('a')
-            ->addSelect('a.status')
+            ->select('a.status')
             ->addSelect('o.id as offer_id')
             ->addSelect('o.name as offer_title')
-            ->addSelect('d.begin as date_start')
+            ->addSelect('ANY_VALUE(d.begin) as date_start')
             ->addSelect('o.maxParticipants as offer_max')
             ->addSelect('COUNT(a.id) as count_applications')
             ->addSelect('COUNT(a.id) / o.maxParticipants as utilization')
@@ -309,9 +306,9 @@ class PassEditionStatistics
             ->where('o.edition = :edition')
             ->setParameter('edition', $passEdition)
             ->andWhere("o.cancelled <> '1'")
-            ->groupBy('a.id', 'a.status', 'd.begin')
+            ->groupBy('o.id', 'a.status')
             ->getQuery()
-            ->execute()
+            ->getScalarResult()
         ;
 
         $labels = [];
@@ -322,23 +319,21 @@ class PassEditionStatistics
         $error = [];
         $waiting = [];
 
-        $offerIds = array_unique(array_column($result, 'offer_id'));
+        $offerIds = array_values(array_unique(array_column($result, 'offer_id')));
 
         foreach ([Attendance::STATUS_CONFIRMED, Attendance::STATUS_WAITLISTED, Attendance::STATUS_ERROR, Attendance::STATUS_WAITING] as $status) {
             $utilizationOfStatus = array_filter($result, fn ($c) => $status === $c['status']);
 
             foreach ($offerIds as $i => $offerId) {
-                $utilizationOfStatusAndOffer = array_filter($utilizationOfStatus, fn ($c) => (int) $c['offer_id'] === (int) $offerId);
-                if ([] !== $utilizationOfStatusAndOffer && null !== $a = array_pop($utilizationOfStatusAndOffer)) {
-                    if (!$a['date_start'] instanceof \DateTimeInterface) {
-                        continue;
-                    }
-
+                $utilizationOfStatusAndOffer = array_values(array_filter($utilizationOfStatus, fn ($c) => (int) $c['offer_id'] === (int) $offerId));
+                if ([] !== $utilizationOfStatusAndOffer &&
+                    null !== ($a = ($utilizationOfStatusAndOffer[0] ?? null))
+                    && !$a['date_start'] instanceof \DateTimeInterface) {
                     $$status[$i] = (float) $a['utilization'];
                     $labels[$i] = sprintf(
                         '%s: %s (max. %d)',
                         $a['offer_title'],
-                        $a['date_start']->format($this->getDateFormat()),
+                        (new DateTime($a['date_start']))->format($this->getDateFormat()),
                         $a['offer_max']
                     );
                     $overall[$i] += (float) $a['utilization'];
@@ -367,7 +362,7 @@ class PassEditionStatistics
         );
 
         $return['labels'] = array_values($labels);
-        foreach (['confirmed', 'waitlisted', 'error', 'waiting'] as $status) {
+        foreach ([Attendance::STATUS_CONFIRMED, Attendance::STATUS_WAITLISTED, Attendance::STATUS_ERROR, Attendance::STATUS_WAITING] as $status) {
             $return['datasets'][] = [
                 'name' => $this->translate('MSC.attendance_status.'.$status),
                 'values' => array_values($$status),
