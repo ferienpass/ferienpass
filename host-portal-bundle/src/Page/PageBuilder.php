45ff27afc5ac967c1b856e8e23c08f8ac32ae95e
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2021-12-01 17:58:29 +0100

    Fix regression bugs

--- host-portal-bundle/src/Page/PageBuilder.php
+++ host-portal-bundle/src/Page/PageBuilder.php
@@ -105,7 +105,7 @@ class PageBuilder
         }
 
         $template->title = strip_tags(Controller::replaceInsertTags($layoutModel->titleTag));
-        $template->description = str_replace(["\n", "\r", '"'], [' ', '', ''], $this->pageModel->description);
+        $template->description = str_replace(["\n", "\r", '"'], [' ', '', ''], (string) $this->pageModel->description);
 
         return $template;
     }
