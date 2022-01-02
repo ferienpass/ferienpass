4fae7b4c5d11f03f030bb0ad752fdc4ff3ea0d67
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-01-02 22:15:13 +0100

    Update Tailwind v3

--- core-bundle/theme/tailwind.config.js
+++ core-bundle/theme/tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-    mode: 'jit',
-    purge: [
+    content: [
         '../templates/Alert/*.html.twig',
         '../templates/Backend/**/*.html.twig',
         '../templates/Form/*.html.twig',
         '../contao/templates/backend/*.html5',
-        './safelist.txt',
+    ],
+    safelist: [
+        'w-2',
+        'h-2',
+        'bg-green-700',
+        'bg-red-700',
+        'inline-block mr-1 bg-yellow-100 rounded-sm leading-none text-xs text-yellow-900 p-1 font-medium',
+        'text-red-900 bg-red-100',
+        'text-green-900 bg-green-100',
+        'flex items-center justify-between'
     ],
     theme: {
         extend: {
