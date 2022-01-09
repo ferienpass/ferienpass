bd0f760ef4a8922ac89c6f81aa9a4b8f2982e6e1
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-01-09 12:59:29 +0100

    Allow changing alias and using prioritized page routes

---
+++ host-portal-bundle/contao/dca/tl_page.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Ferienpass package.
+ *
+ * (c) Richard Henkenjohann <richard@ferienpass.online>
+ *
+ * For more information visit the project website <https://ferienpass.online>
+ * or the documentation under <https://docs.ferienpass.online>.
+ */
+
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_offer_list'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_view_offer'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_edit_offer'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_participant_list'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_view_organization'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_edit_organization'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_personal_data'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_change_password'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_forgot_password'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_privacy_consent'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_follow_invitation'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
