50aaf490bca62c1402c8f1b77e01cd0a68fe6b08
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-01-09 22:31:09 +0100

    Allow to hide/unhide a page

--- core-bundle/contao/dca/tl_page.php
+++ core-bundle/contao/dca/tl_page.php
@@ -11,16 +11,16 @@ declare(strict_types=1);
  * or the documentation under <https://docs.ferienpass.online>.
  */
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['offer_list'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['offer_details'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['host_details'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['applications'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['user_account'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['notifications'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['personal_data'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['account_deleted'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['lost_password'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['lost_password_confirm'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_activate'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_confirm'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
-$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_welcome'] = '{title_legend},title,type;{routing_legend},alias,routePath;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['offer_list'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['offer_details'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['host_details'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['applications'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['user_account'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['notifications'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['personal_data'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['account_deleted'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['lost_password'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['lost_password_confirm'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_activate'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_confirm'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['registration_welcome'] = '{title_legend},title,type;{routing_legend},alias,routePath;{expert_legend:hide},hide;{publish_legend},published';
