508b2cd6b84c7d7d787275547146efcec2eae1d1
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-02-12 18:27:30 +0100

    Add the backup-sftp-bundle

---
+++ backup-sftp-bundle/src/ContaoManager/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Ferienpass package.
+ *
+ * (c) Richard Henkenjohann <richard@ferienpass.online>
+ *
+ * For more information visit the project website <https://ferienpass.online>
+ * or the documentation under <https://docs.ferienpass.online>.
+ */
+
+namespace Ferienpass\BackupSftpBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Ferienpass\BackupSftpBundle\FerienpassBackupSftpBundle;
+use Ferienpass\CoreBundle\FerienpassCoreBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(FerienpassBackupSftpBundle::class)
+                ->setLoadAfter([FerienpassCoreBundle::class, ContaoCoreBundle::class]),
+        ];
+    }
+}
