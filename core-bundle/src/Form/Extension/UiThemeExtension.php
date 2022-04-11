2c42c5c752b794733fb3edbce99fa1d0e6d23693
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-04-11 21:22:46 +0200

    New UI component: "cards" layout for Choices form type

---
+++ core-bundle/src/Form/Extension/UiThemeExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Ferienpass package.
+ *
+ * (c) Richard Henkenjohann <richard@ferienpass.online>
+ *
+ * For more information visit the project website <https://ferienpass.online>
+ * or the documentation under <https://docs.ferienpass.online>.
+ */
+
+namespace Ferienpass\CoreBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UiThemeExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [FormType::class];
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefined(['layout']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['layout'] = $options['layout'] ?? null;
+    }
+}
--- core-bundle/theme/files/js/main.js
+++ core-bundle/theme/files/js/main.js
@@ -1,10 +1,12 @@
-import 'alpinejs'
+import Alpine from 'alpinejs'
 import Gantt from 'frappe-gantt';
 import { Chart } from 'frappe-charts/dist/frappe-charts.esm.js'
+import dragParticipants from './_drag-participants';
 
+window.Alpine = Alpine
 window.Gantt = Gantt;
 window.Chart = Chart;
 
-import dragParticipants from './_drag-participants';
+Alpine.start()
 
 dragParticipants();
