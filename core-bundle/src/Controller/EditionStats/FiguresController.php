3be86d7f710f0069043f9bc25546f4565f3988ad
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-06-21 20:18:38 +0200

    Minor fixes in the Figures Stats Controller

--- core-bundle/src/Controller/EditionStats/FiguresController.php
+++ core-bundle/src/Controller/EditionStats/FiguresController.php
@@ -20,7 +20,7 @@ use Ferienpass\CoreBundle\Repository\EditionRepository;
 use Ferienpass\CoreBundle\Repository\OfferRepository;
 use Symfony\Component\HttpFoundation\Response;
 
-class NumbersController extends AbstractEditionStatsWidgetController
+class FiguresController extends AbstractEditionStatsWidgetController
 {
     public function __construct(private AttendanceRepository $attendanceRepository, private OfferRepository $offerRepository, private EditionRepository $editionRepository)
     {
@@ -30,78 +30,98 @@ class NumbersController extends AbstractEditionStatsWidgetController
     {
         $preceding = $this->editionRepository->findPreceding($edition);
 
-        return $this->render('@FerienpassCore/Backend/EditionStats/numbers.html.twig', [
-            'count_participants' => $this->countParticipants($edition->getId()),
-            'count_participants_preceding' => $this->countParticipants($preceding->getId()),
-            'count_offers' => $this->countOffers($edition->getId()),
-            'count_offers_preceding' => $this->countOffers($preceding->getId()),
-            'count_offers_no_variants' => $this->countOffersWithoutVariants($edition->getId()),
-            'count_offers_no_variants_preceding' => $this->countOffersWithoutVariants($preceding->getId()),
-            'count_hosts' => $this->countHostsWithOffer($edition->getId()),
-            'count_hosts_preceding' => $this->countHostsWithOffer($preceding->getId()),
-            'count_attendances' => $this->countAttendancesWithoutWithdrawn($edition->getId()),
-            'count_attendances_preceding' => $this->countAttendancesWithoutWithdrawn($preceding->getId()),
+        return $this->render('@FerienpassCore/Backend/EditionStats/figures.html.twig', [
+            'count_participants' => $this->countParticipants($edition),
+            'count_participants_preceding' => $this->countParticipants($preceding),
+            'count_offers' => $this->countOffers($edition),
+            'count_offers_preceding' => $this->countOffers($preceding),
+            'count_offers_no_variants' => $this->countOffersWithoutVariants($edition),
+            'count_offers_no_variants_preceding' => $this->countOffersWithoutVariants($preceding),
+            'count_hosts' => $this->countHostsWithOffer($edition),
+            'count_hosts_preceding' => $this->countHostsWithOffer($preceding),
+            'count_attendances' => $this->countAttendancesWithoutWithdrawn($edition),
+            'count_attendances_preceding' => $this->countAttendancesWithoutWithdrawn($preceding),
         ]);
     }
 
-    private function countParticipants(int $editionId): int
+    private function countParticipants(?Edition $edition): ?int
     {
+        if (null === $edition) {
+            return null;
+        }
+
         return (int) $this->attendanceRepository->createQueryBuilder('a')
             ->select('COUNT(DISTINCT COALESCE(IDENTITY(a.participant), a.participantId))')
             ->innerJoin('a.offer', 'o')
             ->andWhere('o.edition = :edition')
-            ->setParameter('edition', $editionId)
+            ->setParameter('edition', $edition->getId())
             ->getQuery()
             ->getSingleScalarResult()
-            ;
+        ;
     }
 
-    private function countAttendancesWithoutWithdrawn(int $editionId): int
+    private function countAttendancesWithoutWithdrawn(?Edition $edition): ?int
     {
+        if (null === $edition) {
+            return null;
+        }
+
         return (int) $this->attendanceRepository->createQueryBuilder('a')
             ->select('COUNT(a.id)')
             ->innerJoin('a.offer', 'o')
             ->andWhere('o.edition = :edition')
-            ->setParameter('edition', $editionId)
+            ->setParameter('edition', $edition->getId())
             ->andWhere('a.status <> :status')
             ->setParameter('status', Attendance::STATUS_WITHDRAWN)
             ->getQuery()
             ->getSingleScalarResult()
-            ;
+        ;
     }
 
-    private function countOffers(int $editionId): int
+    private function countOffers(?Edition $edition): ?int
     {
+        if (null === $edition) {
+            return null;
+        }
+
         return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(o.id) AS count')
             ->andWhere('o.edition = :edition')
-            ->setParameter('edition', $editionId)
+            ->setParameter('edition', $edition->getId())
             ->getQuery()
             ->getSingleScalarResult()
-            ;
+        ;
     }
 
-    private function countOffersWithoutVariants(int $editionId): int
+    private function countOffersWithoutVariants(?Edition $edition): ?int
     {
+        if (null === $edition) {
+            return null;
+        }
+
         return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(o.id) AS count')
             ->andWhere('o.variantBase IS NULL')
             ->andWhere('o.edition = :edition')
-            ->setParameter('edition', $editionId)
+            ->setParameter('edition', $edition->getId())
             ->getQuery()
             ->getSingleScalarResult()
-            ;
+        ;
     }
 
-    private function countHostsWithOffer(int $editionId): int
+    private function countHostsWithOffer(?Edition $edition): ?int
     {
+        if (null === $edition) {
+            return null;
+        }
+
         return (int) $this->offerRepository->createQueryBuilder('o')
             ->select('COUNT(DISTINCT h.id) AS count')
             ->innerJoin('o.hosts', 'h')
             ->andWhere('o.edition = :edition')
-            ->setParameter('edition', $editionId)
+            ->setParameter('edition', $edition->getId())
             ->getQuery()
             ->getSingleScalarResult()
-            ;
+        ;
     }
 }
--- core-bundle/src/Repository/EditionRepository.php
+++ core-bundle/src/Repository/EditionRepository.php
@@ -175,11 +175,16 @@ class EditionRepository extends ServiceEntityRepository
                 Expr\Join::WITH,
                 $qb0->expr()->andX(
                     "period.type = 'holiday'",
-                    $qb0->expr()->lt('period.periodEnd', $edition->getHoliday()->getPeriodEnd()->modify('-10 months')->format('Ymd'))
+                    $qb0->expr()->lt('period.periodBegin', $edition->getHoliday()->getPeriodEnd()->format('Ymd'))
                 )
             )
-            ->orderBy('period.periodEnd', 'DESC')
-            ->getQuery();
+            ->orderBy('period.periodBegin', 'DESC')
+            ->where('edition.id <> :edition_id')
+            ->andWhere('edition.name LIKE :pattern')
+            ->setParameter('edition_id', $edition->getId())
+            ->setParameter('pattern', substr($edition->getName(), 0, 3).'%')
+            ->getQuery()
+        ;
 
         try {
             return $qb->setMaxResults(1)->getOneOrNullResult();
