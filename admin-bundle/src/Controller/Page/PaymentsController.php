5754c69f2988a8002e562d6ee8af90a974a1a7c9
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2023-07-02 18:37:26 +0200

    WIP

--- admin-bundle/src/Controller/Page/PaymentsController.php
+++ admin-bundle/src/Controller/Page/PaymentsController.php
@@ -110,7 +110,7 @@ final class PaymentsController extends AbstractController
         $items = $items->filter(fn (PaymentItem $pi) => $pi->getAttendance()->isPaid());
 
         if ($items->isEmpty()) {
-            $flash->addError('Es wurde nichts storniert. Entweder wurde keine Auswahl getroffen, oder die Buchungen waren schon storniert.');
+            $flash->addError(text: 'Es wurde nichts storniert. Entweder wurde keine Auswahl getroffen, oder die Buchungen waren schon storniert.');
 
             return $this->redirectToRoute('admin_payments_receipt', ['id' => $payment->getId()]);
         }
@@ -127,7 +127,7 @@ final class PaymentsController extends AbstractController
         $em->persist($reversalPayment);
         $em->flush();
 
-        $flash->addConfirmation('Der Stornobeleg wurde erstellt.');
+        $flash->addConfirmation(text: 'Der Stornobeleg wurde erstellt.');
 
         return $this->redirectToRoute('admin_payments_receipt', ['id' => $reversalPayment->getId()]);
     }
